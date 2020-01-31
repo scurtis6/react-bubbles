aff1691bb12bdd83d1af5e6c4c3a1cdf6c4d9aaa
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-31 11:40:09 -0500

    created a login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const [state, setState] = useState({
@@ -14,18 +14,41 @@ const Login = () => {
   const login = e => {
     e.preventDefault();
     axios
-    .post('http://localhost:5000/login', state.credentials)
+    .post('http://localhost:5000/api/login', state.credentials)
     .then(res => {
       console.log(res);
       localStorage.setItem('token', res.data.payload)
+      props.history.push('/bubbles')
     })
     .catch(err => console.log(err))
   }
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className='login'>
+       <h1>Welcome to the Bubble App!</h1>
+      {/* <p>Build a login page here</p> */}
+      <h3>Please Login</h3>
+      <form onSubmit={login}>
+        <label>
+          Username: 
+          <input 
+          type='text'
+          name='username'
+          placeholder='Enter username'
+          value={state.credentials.username}
+          />
+        </label>
+        <label>
+          Password: 
+          <input 
+          type='password'
+          name='password'
+          placeholder='Enter password'
+          value={state.credentials.password}
+          />
+        </label>
+        <button type='sumbit'>Log in</button>
+      </form>
+    </div>
   );
 };
 
